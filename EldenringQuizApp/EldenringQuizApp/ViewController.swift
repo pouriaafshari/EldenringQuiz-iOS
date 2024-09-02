8d1873cd0d628b48eacf3022f978c178d8d7529e
pouriaafshari <pooriyaafshari81@gmail.com>
2024-09-02 18:43:22 +0200

    Initialize the Storyboard and elements

---
+++ EldenringQuizApp/EldenringQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EldenringQuizApp
+//
+//  Created by Kappa on 02/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
